3d815f1481e21ad3ab15b07641da11adb3594e65
Pedro Henrique <phcatanduba@gmail.com>
2021-06-28 19:20:09 -0300

    routers and reset style addeds

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,19 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
 export default function App() {
-    return <main>Hello World</main>;
+    return (
+        <BrowserRouter>
+            <Styles>
+                <Switch>
+                    <Route></Route>
+                </Switch>
+            </Styles>
+        </BrowserRouter>
+    );
 }
+
+const Styles = createGlobalStyle`
+    ${reset}
+`;
