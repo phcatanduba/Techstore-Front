431950c985874c6254ff3b5d223f1345b2a40d19
Mateus_Kuritza <mateuskuritza@gmail.com>
2021-06-29 18:21:46 -0300

    home products

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 import Navbar from "../navbar/Navbar";
+import CategoryContainer from "./CategoryContainer";
 
 export default function Home() {
 	return (
 		<ContainerHome>
 			<Navbar />
+			<CategoryContainer categoryName="mouse" title="Mouses" />
+			<CategoryContainer categoryName="teclado" title="Teclados" />
+			<CategoryContainer categoryName="memoria_ram" title="Memórias" />
 		</ContainerHome>
 	);
 }
